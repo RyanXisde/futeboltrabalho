26a3b9ad8d1b12ec0a257497d6e284890ac8ad15
Ryan <ryan.strider@escola.pr.gov.br>
2025-05-29 11:15:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Você concorda com as diretrizes do site e se comprometerá a seguir os termos de nosso site?");
+
+let nome = prompt("Por gentileza, informe seu nome:");
+
+alert("Bem vindo(a) ao nosso site, " +nome);
